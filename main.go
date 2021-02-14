250a8be00b27780e7ec6233ea9bd90b176f87eca
Bibi Hagan <bibi@Doug.local>
2021-02-14 17:05:26 +1100

    implement new DB

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/mux"
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-memdb"
 )
 
+// global
+var ProdDB *memdb.MemDB
+var OptDB *memdb.MemDB
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+
+	// create DB
+	ProdDB = createProductsDatabase()
+
+	OptDB = createOptionsDatabase()
+
+	handleRequests()
+}
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
@@ -32,38 +44,4 @@ func handleRequests() {
 	myRouter.HandleFunc("/products/{id}/options/{optionId}", deleteOption).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}
-
-func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
-
-	// Open products data file
-	productJSONFILE, err := os.Open("data/products.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened products.json")
-	// defer file close so that file can be parsed
-	defer productJSONFILE.Close()
-
-	// read json file into byte struct
-	productByteValue, _ := ioutil.ReadAll(productJSONFILE)
-	// Unmarshal into to Products struct
-	json.Unmarshal(productByteValue, &Products)
-
-	// Open options data file
-	optionsJSONFILE, err := os.Open("data/options.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened options.json")
-	// defer file close so that file can be parsed
-	defer optionsJSONFILE.Close()
-
-	// read json file into byte struct
-	optionsByteValue, _ := ioutil.ReadAll(optionsJSONFILE)
-	// Unmarshal into to Products struct
-	json.Unmarshal(optionsByteValue, &Options)
-
-	handleRequests()
 }
