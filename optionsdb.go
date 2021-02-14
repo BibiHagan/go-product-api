45220a1258ed2ee1182accc120d39fd2b1df060d
Bibi Hagan <bibi@Doug.local>
2021-02-14 17:04:43 +1100

    create database

---
+++ optionsdb.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-memdb"
+)
+
+func createOptionsDatabase() *memdb.MemDB {
+	//create Option Schema
+	optionSchema := &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{
+			"option": {
+				Name: "option",
+				Indexes: map[string]*memdb.IndexSchema{
+					"id": {
+						Name:    "id",
+						Unique:  true,
+						Indexer: &memdb.StringFieldIndex{Field: "ID"},
+					},
+					"productId": {
+						Name:    "productId",
+						Unique:  false,
+						Indexer: &memdb.StringFieldIndex{Field: "ProductID"},
+					},
+					"name": {
+						Name:    "name",
+						Unique:  false,
+						Indexer: &memdb.StringFieldIndex{Field: "Name"},
+					},
+					"description": {
+						Name:    "description",
+						Unique:  false,
+						Indexer: &memdb.StringFieldIndex{Field: "Description"},
+					},
+				},
+			},
+		},
+	}
+
+	// Create a new product data base
+	optdb, err := memdb.NewMemDB(optionSchema)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a write transaction
+	txn := optdb.Txn(true)
+
+	// Insert some products
+	Options := []*Option{
+		{"1", "1", "Chocolate Croissant", "Chocolate filled croissant"},
+		{"2", "1", "Savoury Croissant", "Ham & cheese filled Croissane"},
+		{"3", "2", "Chocolate Crunt", "Chocolate flavoured Crunt"},
+		{"4", "2", "Apple Crunt", "Apple flavoured Crunt"},
+	}
+	for _, p := range Options {
+		if err := txn.Insert("option", p); err != nil {
+			panic(err)
+		}
+	}
+
+	// Commit the transaction
+	txn.Commit()
+	fmt.Println("Option DB created - go-memdb")
+
+	return OptDB
+}
